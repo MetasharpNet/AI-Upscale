de8ecd7701e74dc1db0559641cb5a583e8974af9
Meta <70144948+metasharp-net@users.noreply.github.com>
2023-06-06 12:48:55 +0200

    Delete realesrgan-ncnn-vulkan directory
    del

--- realesrgan-ncnn-vulkan/cleanup.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo clean inputs folder content + tmp/outputs folders
-del _inputs\*.* /s /q /f > NUL 2> NUL
-rd /S /Q _outputs-animevideov3
-rd /S /Q _outputs-gan-x4plus
-rd /S /Q _outputs-gan-x4plus-anime
-rd /S /Q _tmp
