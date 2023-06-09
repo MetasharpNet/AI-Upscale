cc417672b218cd0ff0d91bc4e1f97ed6baeecf6d
Meta <70144948+metasharp-net@users.noreply.github.com>
2023-06-10 01:08:40 +0200

    Add files via upload
    fix + update

--- realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-pro.cmd
+++ realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-pro.cmd
@@ -9,8 +9,11 @@ del _tmp\*.* /s /q /f > NUL 2> NUL
 mkdir _outputs-pro  > NUL 2> NUL
 del _outputs-pro\*.* /s /q /f > NUL 2> NUL
 realcugan-ncnn-vulkan.exe -x -f png -i _inputs -o _tmp -m models-pro -n 0
+echo resizing and converting to jpg...
 for %%a in ("_tmp\*.png") do (
    call scale.bat -source "%%~fa" -target "_outputs-pro\%%~na.jpg" -max-height 2500 -keep-ratio yes -force yes
 )
-call renumber.cmd _outputs-gan-x4plus-anime\
+echo renumbering files with padding...
+call PowerShell.exe -ExecutionPolicy Bypass -File "padfilenames.ps1" -FolderPath "_outputs-pro"
+echo deleting temporary folder...
 rd /S /Q _tmp
--- realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-se.cmd
+++ realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-se.cmd
@@ -9,8 +9,11 @@ del _tmp\*.* /s /q /f > NUL 2> NUL
 mkdir _outputs-se  > NUL 2> NUL
 del _outputs-se\*.* /s /q /f > NUL 2> NUL
 realcugan-ncnn-vulkan.exe -x -f png -i _inputs -o _tmp -m models-se -n 0
+echo resizing and converting to jpg...
 for %%a in ("_tmp\*.png") do (
    call scale.bat -source "%%~fa" -target "_outputs-se\%%~na.jpg" -max-height 2500 -keep-ratio yes -force yes
 )
-call renumber.cmd _outputs-gan-x4plus-anime\
+echo renumbering files with padding...
+call PowerShell.exe -ExecutionPolicy Bypass -File "padfilenames.ps1" -FolderPath "_outputs-se"
+echo deleting temporary folder...
 rd /S /Q _tmp
