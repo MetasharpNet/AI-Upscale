43d0b818d88179c51bff4349d77032384b4e7a9d
Meta <70144948+metasharp-net@users.noreply.github.com>
2023-06-10 00:01:15 +0200

    Add files via upload
    update

--- realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-pro.cmd
+++ realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-pro.cmd
@@ -8,9 +8,9 @@ echo [models-pro]
 del _tmp\*.* /s /q /f > NUL 2> NUL
 mkdir _outputs-pro  > NUL 2> NUL
 del _outputs-pro\*.* /s /q /f > NUL 2> NUL
-realcugan-ncnn-vulkan.exe -f png -i _inputs -o _tmp -m models-pro -n 0
+realcugan-ncnn-vulkan.exe -x -f png -i _inputs -o _tmp -m models-pro -n 0
 for %%a in ("_tmp\*.png") do (
-   call scale.bat -source "%%~fa" -target "_outputs-pro\%%~nxa" -max-height 2500 -keep-ratio yes -force yes
+   call scale.bat -source "%%~fa" -target "_outputs-pro\%%~na.jpg" -max-height 2500 -keep-ratio yes -force yes
 )
-
+call renumber.cmd _outputs-gan-x4plus-anime\
 rd /S /Q _tmp
--- realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-se.cmd
+++ realcugan-ncnn-vulkan/realcugan-ncnn-vulkan-se.cmd
@@ -8,9 +8,9 @@ echo [models-se] (default)
 del _tmp\*.* /s /q /f > NUL 2> NUL
 mkdir _outputs-se  > NUL 2> NUL
 del _outputs-se\*.* /s /q /f > NUL 2> NUL
-realcugan-ncnn-vulkan.exe -f png -i _inputs -o _tmp -m models-se -n 0
+realcugan-ncnn-vulkan.exe -x -f png -i _inputs -o _tmp -m models-se -n 0
 for %%a in ("_tmp\*.png") do (
-   call scale.bat -source "%%~fa" -target "_outputs-se\%%~nxa" -max-height 2500 -keep-ratio yes -force yes
+   call scale.bat -source "%%~fa" -target "_outputs-se\%%~na.jpg" -max-height 2500 -keep-ratio yes -force yes
 )
-
+call renumber.cmd _outputs-gan-x4plus-anime\
 rd /S /Q _tmp
