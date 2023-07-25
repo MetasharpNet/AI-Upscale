bf27459ae2091649f937dd40ca1bd6f0959c9d77
Metasharp <70144948+MetasharpDev@users.noreply.github.com>
2023-07-25 19:02:31 +0200

    Add files via upload
    now _inputs takes a list of RAR, ZIP, CBZ and CBR files and outputs a list of CBZ files

---
+++ realesrgan-ncnn-vulkan/zip.ps1
@@ -0,0 +1,30 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$SourceFolder,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$DestinationFilePath
+)
+
+if (-not (Test-Path $SourceFolder)) {
+    Write-Host "Source folder not found: $SourceFolder"
+    exit 1
+}
+
+if (-not (Test-Path (Split-Path -Parent $DestinationFilePath))) {
+    Write-Host "Destination filepath not found: $(Split-Path -Parent $DestinationFilePath)"
+    exit 1
+}
+
+# Append ".zip" to the destination path temporarily
+$DestinationFilePathAsZip = $DestinationFilePath + ".zip"
+
+# Compress the folder into a ZIP archive
+Compress-Archive -Path $SourceFolder -DestinationPath $DestinationFilePathAsZip -CompressionLevel Optimal
+
+# Create the full path for the final ".cbz" file
+$DestinationFolder=Split-Path $DestinationFilePath
+$DestinationFilename=Split-Path $DestinationFilePath -Leaf
+
+# Rename the ".zip" file to ".cbz"
+Rename-Item -Path $DestinationFilePathAsZip -NewName ($DestinationFilename)
